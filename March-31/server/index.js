2ab004b8d8835baed2850ebddfe02f7a0cef4a53
Jahnavi Krishna K <kjahnavik@gmail.com>
2022-04-21 23:43:47 +0530

    Add files via upload

---
+++ March-31/server/index.js
@@ -0,0 +1,21 @@
+const WebSocket=require('ws')
+const wss=new WebSocket.Server({port:8080})
+wss.on("connection",ws=>{
+    console.log("new client is added");
+    // callbaback function that will be called whenever a new client is added
+    // connection is an event - to establish the socket connection
+    ws.on("message",data=>{
+        console.log(`something data is recived:${data}`);
+        const info=JSON.parse(data);
+        info.forEach(element => {
+            element.greet=`hello ${element.name}`
+        });
+        
+        ws.send(JSON.stringify(info));
+        
+    
+    })
+    ws.on("close",()=>{
+        console.log("client is disconnected");
+    })
+})
